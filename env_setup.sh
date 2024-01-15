2311c1e8500817d44e2a861d0cd5211bf6132ef9
cihanb <cihanb@live.com>
2024-01-14 18:24:49 -0800

    updated python check

--- env_setup.sh
+++ env_setup.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+
+# Check for Python installation
+if [[ -x "$(command -v python3)" ]]; then
+  echo "Python is installed."
+else
+  echo "Python is not installed."
+  echo "Please visit https://www.python.org/downloads/ to install it."
+  exit 1
+fi
+
+# Install dependencies
 pip install python-dotenv
 pip install requests
 pip install pandas
